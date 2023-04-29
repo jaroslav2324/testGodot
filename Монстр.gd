8310bda7574889e873479ea99becf84527204eea
Gleb-Yakovlev <100411117+Gleb-Yakovlev@users.noreply.github.com>
2023-04-29 20:49:38 +0300

    glebTimer

--- Монстр.gd
+++ Монстр.gd
@@ -1,12 +1,20 @@
 extends RigidBody2D
 
+var anger = 0
 @export var monsterSpeed = 100
 func _ready():
 	self.position.x = -400
 	self.position.y = 100
+	$monster/TimerAnger.start()
 
 func _process(delta):
 	self.linear_velocity = Vector2(monsterSpeed,0)
 	$monster/AnimatedSprite2D.play("fly")
 	
+	if anger == 3:
+		get_tree().paused = true
 	
+
+
+func _on_timer_anger_timeout():
+	anger = anger - 1
